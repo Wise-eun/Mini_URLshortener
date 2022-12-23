765d5f07ec7df6fd240ae06cc865923fd6bd922a
Wise-eun <67956564+Wise-eun@users.noreply.github.com>
2022-12-24 02:33:57 +0900

    feat : URL리스트 아이템 클릭시, 압축된 해당 URL링크 복사기능 구현

--- app/src/main/java/com/example/sgdevcmap_urlshortener/Adapter.java
+++ app/src/main/java/com/example/sgdevcmap_urlshortener/Adapter.java
@@ -1,9 +1,14 @@
 package com.example.sgdevcmap_urlshortener;
 
+import static android.content.Context.CLIPBOARD_SERVICE;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +18,8 @@ import java.util.ArrayList;
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     private ArrayList<ListData> urllistdata;
+
+
 
     public Adapter(ArrayList<ListData> urllistdata)
     {
@@ -45,9 +52,31 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder{
         private TextView textView_item_origin;
         private  TextView textView_item_shorten;
+
+
 public ViewHolder(@NonNull View itemView)
 {
+
     super(itemView);
+
+    itemView.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int pos = getAdapterPosition();
+            if(pos != RecyclerView.NO_POSITION){
+                ListData data = urllistdata.get(pos);
+                ClipboardManager clipboardManager = (ClipboardManager) view.getContext().getSystemService(CLIPBOARD_SERVICE);
+                ClipData clipData = ClipData.newPlainText("URL", data.getTextview_shortenURL());
+                clipboardManager.setPrimaryClip((clipData));
+            
+            
+                Toast.makeText(view.getContext(), "URL이 복사되었습니다.", Toast.LENGTH_SHORT).show();
+
+
+            }
+        }
+    });
+
     textView_item_origin = (TextView) itemView.findViewById(R.id.item_originURL);
     textView_item_shorten = (TextView) itemView.findViewById(R.id.item_shortenURL);
 }
